0a91a72aa9da5c2ed5f9f266751758bdb2e567a3
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-10-05 21:12:01 +0530

    input statement

--- src/StringHandling/Ques7.java
+++ src/StringHandling/Ques7.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Ques7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String s;
+        s = sc.nextLine();
 
     }
 
