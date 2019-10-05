0fef091334bdb2c1723dcffb18ce7bcb2aa355fd
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-10-05 21:26:58 +0530

    set input method

--- src/StringHandling/Ques10.java
+++ src/StringHandling/Ques10.java
@@ -1,7 +1,10 @@
 package StringHandling;
 
+import java.util.Scanner;
+
 public class Ques10 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
