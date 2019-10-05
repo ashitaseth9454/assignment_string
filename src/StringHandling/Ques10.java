bb9a00e4f697ba18798ce58a41d477b0bf35c0a5
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-10-05 21:30:10 +0530

    set main method

--- src/StringHandling/Ques10.java
+++ src/StringHandling/Ques10.java
@@ -6,7 +6,11 @@ public class Ques10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s;
+        System.out.println("enter string");
         s = sc.nextLine();
+        System.out.println("enter character");
+        char c = sc.nextLine().charAt(0);
+        System.out.println("index " + s.indexOf(c));
 
     }
 }
