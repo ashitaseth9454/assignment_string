ce4602256cbf31b1614d1a2b9e1282d95bab3d46
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-10-05 21:20:00 +0530

    set main method

--- src/StringHandling/Ques8.java
+++ src/StringHandling/Ques8.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class Ques8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String a;
+        a = sc.nextLine();
+        int upperWeight = 0;
+        for (int i = 0; i < a.length(); i++) {
+            char t = a.charAt(i);
+            if (t >= 65 && t <= 90) {
+                upperWeight = upperWeight + t;
+            }
 
+        }
+        System.out.println("UPPER WEIGHT=" + upperWeight);
     }
 }
---
+++ src/StringHandling/Ques9.java
@@ -0,0 +1,7 @@
+package StringHandling;
+
+public class Ques9 {
+    public static void main(String[] args) {
+
+    }
+}
