8fd690ea5d04124b13af18c79f5dc4046f3f4db5
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2019-10-05 20:49:56 +0530

    main method

--- src/StringHandling/Ques3.java
+++ src/StringHandling/Ques3.java
@@ -5,5 +5,17 @@ import java.util.Scanner;
 public class Ques3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String s1;
+
+        s1 = sc.nextLine();
+        s1 = s1.replace('a', '*');
+        s1 = s1.replace('e', '*');
+        s1 = s1.replace('i', '*');
+        s1 = s1.replace('o', '*');
+        s1 = s1.replace('u', '*');
+        System.out.println(s1);
+
+
     }
-}
+    }
+
---
+++ src/StringHandling/Ques4.java
@@ -0,0 +1,7 @@
+package StringHandling;
+
+public class Ques4 {
+    public static void main(String[] args) {
+
+    }
+}
